b826e0aac1743b7265d5f206d70761b832e51a52
badwaikparas <badwaikparas@gmail.com>
2025-02-20 00:10:27 +0530

    conversation with pdf code

---
+++ testing_py/script1.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Hello from script1!")
+
+if __name__ == "__main__":
+    print("script1 is running directly")
---
+++ testing_py/script2.py
@@ -0,0 +1,6 @@
+import script1
+import subprocess
+
+# script1.greet()  # Calling a function from script1import subprocess
+
+subprocess.run(["python", "./testing_py/script1.py"])
